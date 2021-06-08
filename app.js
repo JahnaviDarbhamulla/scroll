982b7262f14c4b3082b758a2bc875d66162f3d4f
Jahnavi Darbhamulla <76477365+JahnaviDarbhamulla@users.noreply.github.com>
2021-06-08 19:09:59 +0530

    initial files

---
+++ app.js
@@ -0,0 +1,14 @@
+gsap.to('#moon',{
+  ScrollTrigger:{
+    scrub:1,
+  },
+  scale: 1.5,
+})
+// gsap.to('#bg';{
+//   ScrollTrigger:{
+//     scrub:1,
+//   },
+//   scale: 1.5,
+//   y: -500,
+//   x: 2200,
+// })
